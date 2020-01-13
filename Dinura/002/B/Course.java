9e4a8d4a4033b638ac38cf81f5e9ebf6d7b13845
Dinura Seneviratne <dinuras@gmail.com>
2020-01-13 06:44:03 +0530

    Initial commit

---
+++ Dinura/002/B/Course.java
@@ -0,0 +1,8 @@
+package com.dinura.directories;
+
+public class Course{
+	
+	public void print(){
+		System.out.println("Hello Java");
+	}
+}
---
+++ Dinura/002/C/Student.java
@@ -0,0 +1,11 @@
+package com.dinura.directories;
+import com.dinura.directories.Course;
+
+public class Student{
+	
+	Course course = new Course();
+
+	public void print(){
+		Course.print();
+	}
+}
